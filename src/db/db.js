a24326dd4f0791d251ccaf1f2024fe12c314a817
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2024-06-04 18:59:21 +0530

    modify mongo connection

--- src/db/db.js
+++ src/db/db.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDatabase = async function () {
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then((e) => console.log("MongoDB Connect"))
-    .catch((err) => console.error("MongoDB connection error:", err));
-  //   await mongoose.connect(process.env.MONGO_URL, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //   });
-  //   console.log("Connected to MongoDB with Mongoose");
+  // mongoose
+  //   .connect(process.env.MONGO_URL)
+  //   .then((e) => console.log("MongoDB Connect"))
+  //   .catch((err) => console.error("MongoDB connection error:", err));
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Increase connection timeout
+    socketTimeoutMS: 45000, // Increase socket timeout
+  });
+  console.log("Connected to MongoDB with Mongoose");
 };
 
 module.exports = { connectDatabase };
-
-
