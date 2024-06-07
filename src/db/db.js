0eb7fed3f931f0463f3056521e300a0f8b2ace3b
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2024-06-07 21:24:01 +0530

    some fixes

--- src/db/db.js
+++ src/db/db.js
@@ -2,16 +2,16 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDatabase = async function () {
-  // mongoose
-  //   .connect(process.env.MONGO_URL)
-  //   .then((e) => console.log("MongoDB Connect"))
-  //   .catch((err) => console.error("MongoDB connection error:", err));
-  await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    connectTimeoutMS: 30000, // Increase connection timeout
-    socketTimeoutMS: 45000, // Increase socket timeout
-  });
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then((e) => console.log("MongoDB Connect"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+  // await mongoose.connect(process.env.MONGO_URL, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   connectTimeoutMS: 30000, // Increase connection timeout
+  //   socketTimeoutMS: 45000, // Increase socket timeout
+  // });
   console.log("Connected to MongoDB with Mongoose");
 };
 
